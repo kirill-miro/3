135e474c96a86aa000eee8f92709df2b6ed931b7
kirill-miro <mirosnicenkokirill615@gmail.com>
2025-05-27 21:28:33 +0300

    home work 1

--- kirill.py
+++ kirill.py
@@ -1,1 +1,8 @@
-print("hello word")
+password = "321"
+
+user_input = input(" Введіть пароль: ")
+
+if user_input == password:
+    print("Пароль вірний! Доступ дозволено.")
+else:
+    print("Невірний пароль. Спробуйте ще раз.")
