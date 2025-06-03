a30602796edc70b5711cd968f491e56438ca0260
kirill-miro <mirosnicenkokirill615@gmail.com>
2025-06-03 09:57:44 +0300

    home work 2

--- 4.py
+++ 4.py
@@ -1,35 +1,65 @@
-print("Привіт! Вітаю у грі 'Вікторина'.")
-print("Тобі буде поставлено кілька запитань. Введи відповідь та отримуй бали!")
-print("Відповідай точно! Почнімо.\n")
+import tkinter as tk
+from tkinter import messagebox
+import random
 
-score = 0
+class TicTacToeGame:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Хрестики-нулики: Гравець vs Бот")
+        self.board = ["" for _ in range(9)]
+        self.buttons = []
+        self.create_widgets()
 
-q11 = input("1. Яка столиця України? ")
-if q11 == "київ":
-    print("Правильно!!!\n")
-    score += 1
-else:
-    print("Неправильно! Правильна відповідь: Київ\n")
+    def create_widgets(self):
+        for i in range(9):
+            button = tk.Button(self.root, text="", font=('consolas', 40), width=5, height=2,
+                               command=lambda i=i: self.player_move(i))
+            button.grid(row=i // 3, column=i % 3)
+            self.buttons.append(button)
 
-q21 = input("2. Скільки буде 5 + 7? ")
-if q21 == "12":
-    print("Правильно!!!\n")
-    score += 1
-else:
-    print("Неправильно! Правильна відповідь: 12\n")
+    def player_move(self, index):
+        if self.board[index] == "" and not self.check_winner():
+            self.board[index] = "X"
+            self.buttons[index].config(text="X")
+            if self.check_winner():
+                messagebox.showinfo("Результат", "Ти переміг!")
+                self.reset_game()
+            elif "" not in self.board:
+                messagebox.showinfo("Результат", "Нічия!")
+                self.reset_game()
+            else:
+                self.bot_move()
 
-q22 = input("3. Яка планета найближча до Сонця? ")
-if q22 == "меркурій":
-    print("Правильно!!!\n")
-    score += 1
-else:
-    print("Неправильно! Правильна відповідь: Меркурій\n")
+    def bot_move(self):
+        empty = [i for i, val in enumerate(self.board) if val == ""]
+        if empty:
+            move = random.choice(empty)
+            self.board[move] = "O"
+            self.buttons[move].config(text="O")
+            if self.check_winner():
+                messagebox.showinfo("Результат", "Бот переміг!")
+                self.reset_game()
+            elif "" not in self.board:
+                messagebox.showinfo("Результат", "Нічия!")
+                self.reset_game()
 
-q23 = input("4. Що таке інерція? ")
-if q23 == "Здатність тіла зберігати стан спокою або рівномірного руху":
-    print("правильно!!!\n")
-    score += 1
-else:
-    print("Неправильно! Правильна відповідь: Меркурій\n")
+    def check_winner(self):
+        wins = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ]
+        for line in wins:
+            if self.board[line[0]] == self.board[line[1]] == self.board[line[2]] != "":
+                return self.board[line[0]]
+        return None
 
-    print(f"Гру завершено! Твій результат: {score} з 4 правильних відповідей.")
+    def reset_game(self):
+        self.board = ["" for _ in range(9)]
+        for button in self.buttons:
+            button.config(text="")
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    game = TicTacToeGame(root)
+    root.mainloop()
